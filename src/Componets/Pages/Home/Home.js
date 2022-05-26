54b8684399eb6af1c9feaa916a6b8cecca9ed6c2
sohel Ahamed <sohel770hossain@gmail.com>
2022-05-26 17:54:00 +0600

    commit

--- src/Componets/Pages/Home/Home.js
+++ src/Componets/Pages/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import Brands from '../Brands/Brands';
+import Products from '../Products/Products';
+import Summary from '../Summary/Summary';
 
 const Home = () => {
     return (
         <div>
-            <h2>this is Home</h2>
+          <Banner></Banner>
+          <Products></Products>
+          <Summary></Summary>
+          <Brands></Brands>
         </div>
     );
 };
