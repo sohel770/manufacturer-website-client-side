8220aa7377c17908526e72c6d1765adf2d0410e8
sohel Ahamed <sohel770hossain@gmail.com>
2022-05-26 18:49:27 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Componets/Pages/Blogs/Blogs';
 import Home from './Componets/Pages/Home/Home';
+import Login from './Componets/Pages/Login/Login';
+import Register from './Componets/Pages/Register/Register';
+import Footer from './Componets/Shared/Footer/Footer';
+// import Register from './Componets/Pages/Register/Register';
 import Header from './Componets/Shared/Header/Header'
 
 
@@ -9,9 +14,14 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-    <Routes>
-     <Route path='/' element={<Home></Home>}></Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+       <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+       {/* <Route path='/'></Route> */}
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
